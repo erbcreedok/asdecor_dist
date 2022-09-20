9bfd458cf2b4e23feac6cc327782f7e4902bf8ed
erbcreedok <erbcreedok@gmail.com>
2022-09-20 23:41:34 +0600

    add footer in product page

--- product.php
+++ product.php
@@ -143,6 +143,15 @@ $images = ['https://images.satu.kz/157800228_w640_h640_slivki-naturalnye-mila.jp
     </div>
     <?php endif; ?>
 
+    <footer class="py-3">
+        <div class="burger">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        ПРИЯТНЫХ ПОКУПОК!
+    </footer>
+
 <?php endif; ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
